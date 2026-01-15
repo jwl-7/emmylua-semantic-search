0dd5b121e158e6958e6605922e78d7101e22384a
jwl-7 <lucidrainbows@gmail.com>
2026-01-15 12:16:41 -0500

    build v3 of v1, which is v2 of v1, which is v1 of v1 so.. I guess this is v1

--- emmylua-search/vite.config.ts
+++ emmylua-search/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: "/emmylua-search/",
+    base: "/emmylua-semantic-search/",
     resolve: { alias: { '@': path.resolve(__dirname, './src') } },
     build: { outDir: '../docs' }
 })
