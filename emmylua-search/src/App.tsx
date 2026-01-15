d5884035ded0dc6e799edc2e23d322c45ef2992a
jwl-7 <lucidrainbows@gmail.com>
2026-01-15 05:13:48 -0500

    expand syntax highlighting

--- emmylua-search/src/App.tsx
+++ emmylua-search/src/App.tsx
@@ -12,17 +12,32 @@ interface LuaKBEntry {
 
 const LuaHighlighter = ({ code }: { code: string }) => {
     const highlighted = useMemo(() => {
-        let h = code.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-        h = h.replace(/(---@.*)/g, '<span class="lua-cyan">$1</span>');
-        h = h.replace(/\b(function|end)\b(?![^<]*>)/g, '<span class="lua-red">$1</span>');
-        h = h.replace(/(lua-red">function<\/span>\s+)([\w\.:]+)/g, '$1<span class="lua-green">$2</span>');
+        let h = code.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;")
+
+        // @param
+        h = h.replace(/(---@param\s+)([a-zA-Z_]\w*)(\s+.*)/g, (match, tag, name, type) => {
+            return `<span class="lua-cyan">${tag}</span><span class="lua-orange">${name}</span><span class="lua-cyan">${type}</span>`
+        })
+
+        // @tags
+        h = h.replace(/(---@(?!param).+)/g, '<span class="lua-cyan">$1</span>')
+
+        // keywords
+        h = h.replace(/\b(function|end)\b(?![^<]*>)/g, '<span class="lua-red">$1</span>')
+
+        // function name
+        h = h.replace(/(lua-red">function<\/span>\s+)([\w\.:]+)/g, '$1<span class="lua-green">$2</span>')
+
+        // parenthesis content
         h = h.replace(/(\()([^)]*)(\))/g, (match, open, content, close) => {
-            if (open.includes('<') || content.includes('<')) return match;
-            const orangeParams = content.replace(/\b([a-zA-Z_]\w*)\b/g, '<span class="lua-orange">$1</span>');
-            return `${open}${orangeParams}${close}`;
-        });
-        return h;
-    }, [code]);
+            if (open.includes('<') || content.includes('<')) return match
+            const orangeParams = content.replace(/\b([a-zA-Z_]\w*)\b/g, '<span class="lua-orange">$1</span>')
+            return `${open}${orangeParams}${close}`
+        })
+
+        return h
+    }, [code])
+
     return <code dangerouslySetInnerHTML={{ __html: highlighted }} />
 }
 
